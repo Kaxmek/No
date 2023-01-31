ef43c41f946315d4c66ef523a397ab32edb11509
حلـم <111011410+Kaxmek@users.noreply.github.com>
2023-01-31 21:15:54 +0300

    Update fifthon.py

--- fifthon.py
+++ fifthon.py
@@ -142,7 +142,7 @@ ownerhson_id = 1041483862
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerhson_id :
-        order = await event.reply('اهلا مطوري حلم @iziii')
+        order = await event.reply('●━━━━━━ @HLLAM ━━━━━━●')
 
 
 @fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
