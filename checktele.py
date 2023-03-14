1d7834b2fd0504290bfafb1042c2e131589bcd02
manafOa <121410577+manafOa@users.noreply.github.com>
2023-03-14 17:02:23 +0000

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -270,7 +270,7 @@ async def _(event):
                     if "A wait of" in str(eee):
                         break
                     else:
-                        await fifthon.send_message(event.chat.id, " اجاك متاح !")
+                        await fifthon.send_message(event.chat.id, " الحق جاك يوزر متاح !")
             else:
                 pass
             trys += 1
